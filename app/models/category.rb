8725d782fc34c72b8f89d86e8eecbeda981513ca
MorpheusVibez <halodre24@gmail.com>
2020-07-01 22:15:33 -0400

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
+  #Creates Methods:
+  # post_categories
+  # post_categories<<
+  # post_categories.delete
+  # post_category_ids
+
+
   has_many :posts, through: :post_categories
+  #category.ids
 end
